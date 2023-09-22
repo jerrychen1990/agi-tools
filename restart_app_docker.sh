46b875f5beb975a7ea443f850bb5eeb274f854e9
jerrychen1990 <jerrychen1990@gmail.com>
2023-09-22 16:38:47 +0800

    添加脚本

--- restart_app_docker.sh
+++ restart_app_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'starting service'
-streamlit run app.py --server.port 8501 | tee $LOG_HOME/st_tool.log
+streamlit run app.py --server.port 8501 --logger.level=debug 2>&1 | tee $LOG_HOME/st_tool.log
--- run_docker_service.sh
+++ run_docker_service.sh
@@ -1,6 +1,7 @@
 TAG=${TAG}
 echo "stoping old service"
 sudo docker ps | grep agi-tools | awk '{print $1}' | xargs docker stop
+sleep 1
 echo "starting new service"
 target=agi-tools:$TAG
 cmd="sudo docker run -idt -p8501:8501 $target"
